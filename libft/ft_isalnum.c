96de2bbce0af751c510ff4098d13677fab534af3
James Jung <therealjamesjung@gmail.com>
2021-11-09 16:57:54 +0900

    [libft] ft_isalnum.c

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -6,12 +6,12 @@
 /*   By: jaekjung <jaekjung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 16:47:31 by jaekjung          #+#    #+#             */
-/*   Updated: 2021/11/09 16:54:59 by jaekjung         ###   ########.fr       */
+/*   Updated: 2021/11/09 16:57:30 by jaekjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_isalpha(int c);
-int ft_isdigit(int c);
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
 
 int	ft_isalnum(int c)
 {
